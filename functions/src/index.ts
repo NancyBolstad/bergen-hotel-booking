7eaef119246ec7b28221708191d49f773ff9556c
NancyBolstad <litchacha@gmail.com>
2020-04-25 16:05:10 +0200

    Test create a new user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,58 @@
 import * as functions from 'firebase-functions';
-// import * as admin from 'firebase-admin';
-// import * as express from 'express';
-// import * as bodyParser from 'body-parser';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-import getRootUrls from './utils/getRootUrls';
+admin.initializeApp(functions.config().firebase);
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const start = functions.https.onRequest((request, response) => {
-  response.send({
-    message: 'Welcome to the Holidaze hotel booking API',
-    versions: [
-      {
-        version: 1,
-        link: `${getRootUrls(request)}/api/v1`,
-      },
-    ],
-  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+export const api = functions.https.onRequest(app);
+
+const db = admin.firestore();
+const userCollection = 'users';
+
+interface User {
+  firstName: String;
+  lastName: String;
+}
+
+// Create new user
+app.post('/users', async (req, res) => {
+  try {
+    const user: User = {
+      firstName: req.body['firstName'],
+      lastName: req.body['lastName'],
+    };
+
+    const newDoc = await db.collection(userCollection).add(user);
+
+    console.log(newDoc);
+    res.status(201).send(`Created a new user: ${newDoc.id}`);
+  } catch (error) {
+    res
+      .status(400)
+      .send(
+        `User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`,
+      );
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const userQuerySnapshot = await db.collection(userCollection).get();
+    const users: any[] = [];
+    userQuerySnapshot.forEach((doc) => {
+      users.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });

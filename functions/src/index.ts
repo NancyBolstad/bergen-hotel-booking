6fcb8e620c5118b3b57fd9db85600a2e779951b5
NancyBolstad <litchacha@gmail.com>
2020-04-25 11:15:11 +0200

    Test hello world and it works now

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,21 @@
 import * as functions from 'firebase-functions';
+// import * as admin from 'firebase-admin';
+// import * as express from 'express';
+// import * as bodyParser from 'body-parser';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import getRootUrls from './utils/getRootUrls';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const start = functions.https.onRequest((request, response) => {
+  response.send({
+    message: 'Welcome to the Holidaze hotel booking API',
+    versions: [
+      {
+        version: 1,
+        link: `${getRootUrls(request)}/api/v1`,
+      },
+    ],
+  });
+});

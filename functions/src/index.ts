530bd7be06a380ca27bdb93c5a52bb400ec6f0e0
NancyBolstad <litchacha@gmail.com>
2020-04-26 14:48:51 +0200

    Test firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as firebase from 'firebase';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-
-admin.initializeApp(functions.config().firebase);
 
 const app = express();
 
@@ -12,46 +10,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 export const api = functions.https.onRequest(app);
 
-const db = admin.firestore();
-const userCollection = 'users';
+const config = {
+  apiKey: 'AIzaSyBEqQ8JXTaRWlPxmZ-yFmu8-gUj2UCQP2k',
+  authDomain: 'holidaze-service.firebaseapp.com',
+  databaseURL: 'https://holidaze-service.firebaseio.com',
+  projectId: 'holidaze-service',
+  storageBucket: 'holidaze-service.appspot.com',
+  messagingSenderId: '957594338325',
+  appId: '1:957594338325:web:388025c77f62b70bbf7a1c',
+  measurementId: 'G-6PBETGE2D7',
+};
 
-interface User {
-  firstName: String;
-  lastName: String;
-}
+firebase.initializeApp(config);
+const database = firebase.database();
 
-// Create new user
-app.post('/users', async (req, res) => {
+app.get('/contact', async (req, res) => {
   try {
-    const user: User = {
-      firstName: req.body['firstName'],
-      lastName: req.body['lastName'],
-    };
-
-    const newDoc = await db.collection(userCollection).add(user);
-
-    console.log(newDoc);
-    res.status(201).send(`Created a new user: ${newDoc.id}`);
-  } catch (error) {
-    res
-      .status(400)
-      .send(
-        `User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`,
-      );
-  }
-});
-
-app.get('/users', async (req, res) => {
-  try {
-    const userQuerySnapshot = await db.collection(userCollection).get();
-    const users: any[] = [];
-    userQuerySnapshot.forEach((doc) => {
-      users.push({
-        id: doc.id,
-        data: doc.data(),
+    return database
+      .ref('contact')
+      .once('value')
+      .then(function (snapshot) {
+        res.status(200).json({ contact: snapshot.val() });
       });
-    });
-    res.status(200).json(users);
   } catch (error) {
     res.status(500).send(error);
   }

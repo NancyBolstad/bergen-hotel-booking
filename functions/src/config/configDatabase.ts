b9ea4403a354928bc5d46c79005853297fa01ea9
NancyBolstad <litchacha@gmail.com>
2020-04-26 16:24:25 +0200

    Refactor config for firebase

---
+++ functions/src/config/configDatabase.ts
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: 'AIzaSyBEqQ8JXTaRWlPxmZ-yFmu8-gUj2UCQP2k',
+  authDomain: 'holidaze-service.firebaseapp.com',
+  databaseURL: 'https://holidaze-service.firebaseio.com',
+  projectId: 'holidaze-service',
+  storageBucket: 'holidaze-service.appspot.com',
+  messagingSenderId: '957594338325',
+  appId: '1:957594338325:web:388025c77f62b70bbf7a1c',
+  measurementId: 'G-6PBETGE2D7',
+};
+
+export default config;

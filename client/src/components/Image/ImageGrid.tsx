4a198c0b678a5ed8661441f4b19f40012d5f0e29
NancyBolstad <litchacha@gmail.com>
2020-05-10 19:22:30 +0200

    Clean

--- client/src/components/Image/ImageGrid.tsx
+++ client/src/components/Image/ImageGrid.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import { Image } from '../../types/types';
 import { Grid, GridItem, WidthConstraints, VerticalSpacer, HorizontalSpacer } from '../Layout';
 

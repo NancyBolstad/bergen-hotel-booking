06c97a1fbcd085c9a0f35956fae341a05e2d4acf
NancyBolstad <litchacha@gmail.com>
2020-05-13 13:07:29 +0200

    Added like button

--- client/src/components/HotelCards/HotelCardVariant.tsx
+++ client/src/components/HotelCards/HotelCardVariant.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import Typography from '../Typography';
 import transformLangText from '../../util/transformLangText';
-import { CardVariant, FeaturedImages, CardVariantLeft, CardVariantRight } from './styles';
+import {
+  CardVariant,
+  FeaturedImages,
+  CardVariantLeft,
+  CardVariantRight,
+  LikeButton,
+  LikeButtonWrapper,
+} from './styles';
 import { HotelDetails } from '../../types/response';
 import useIsMobile from '../../hooks/useIsMobile';
+import { heart, heartSolid } from '../../util/icons';
+import { Context } from '../../context/GlobalContext';
+import { Types } from '../../reducer/favoriteCardsReducer';
 
 interface Props {
   card: HotelDetails;
@@ -11,8 +21,32 @@ interface Props {
 
 const HotelCardVariant: React.FunctionComponent<Props> = ({ card }) => {
   const isMobile = useIsMobile();
+  const { favorites, dispatch } = React.useContext(Context);
+  const [like, setLike] = React.useState<boolean>(() => {
+    const found = favorites.find(item => {
+      return item.id === card.id;
+    });
 
-  console.log(22222222222222222887779);
+    return found ? true : false;
+  });
+
+  function handleLikeDispatch() {
+    setLike(!like);
+
+    if (!like === true) {
+      dispatch({
+        type: Types.Like,
+        payload: card,
+      });
+    } else {
+      dispatch({
+        type: Types.Dislike,
+        payload: {
+          id: card.id,
+        },
+      });
+    }
+  }
 
   return (
     <CardVariant to={`/accommodation/details/${card.id}`}>
@@ -22,6 +56,19 @@ const HotelCardVariant: React.FunctionComponent<Props> = ({ card }) => {
         </CardVariantLeft>
       )}
       <CardVariantRight>
+        <LikeButtonWrapper>
+          <LikeButton
+            variant="primary"
+            size="small"
+            isLiked={like}
+            onClick={e => {
+              e.preventDefault();
+              handleLikeDispatch();
+            }}
+          >
+            {like ? heartSolid : heart}
+          </LikeButton>
+        </LikeButtonWrapper>
         {!!card.category && <Typography element="span" variant="b2" content={card.category} />}
         {!!card.name && <Typography element="span" variant="b2" content={card.name} />}
         {!!card.descriptions && (

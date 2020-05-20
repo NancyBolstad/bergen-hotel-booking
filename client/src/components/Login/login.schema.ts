2b4aac6584e043c024da521b2a34beeaf9580b50
NancyBolstad <litchacha@gmail.com>
2020-05-20 14:30:59 +0200

    Edited login schema field

--- client/src/components/Login/login.schema.ts
+++ client/src/components/Login/login.schema.ts
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
 
 const LoginSchema: Yup.ObjectSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email')
+  userName: Yup.string()
+    .min(6, 'Too Short!')
+    .max(8, 'Too Long!')
     .required('Required'),
   password: Yup.string()
     .min(6, 'Too Short!')

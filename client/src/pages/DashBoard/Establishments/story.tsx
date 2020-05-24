b5116492815f5116bc43964ff9efe06fddb186e4
NancyBolstad <litchacha@gmail.com>
2020-05-24 22:49:42 +0200

    Clean

--- client/src/pages/DashBoard/Establishments/story.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import EstablishmentForm from './EstablishmentForm';
-
-storiesOf('Component/EstablishmentForm', module).add('Default', () => <EstablishmentForm />);

f7d4863e7f87062756f514ac0cb841a9142a5f49
NancyBolstad <litchacha@gmail.com>
2020-05-26 18:28:44 +0200

    Fixed error import

--- client/src/components/IconList/stories.tsx
+++ client/src/components/IconList/stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import IconList from '.';
+import IconList from './IconList';
 
 storiesOf('Component/IconList', module).add('Default', () => <IconList />);

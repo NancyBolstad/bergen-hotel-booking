a786b69cd3111305ee3e30129b60ccbc3f9f3cea
NancyBolstad <litchacha@gmail.com>
2020-05-26 23:43:06 +0200

    Added main content warpper

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -12,6 +12,7 @@ import Blog from './Blog/Blog';
 import BlogDetails from './Blog/BlogDetails';
 import PrivateRoute from '../routes/PrivateRoute';
 import DashBoard from './Dashboard/Dashboard';
+import MainContent from '../components/MainContent';
 
 const Contact = lazy(() => import('./Contact'));
 const Book = lazy(() => import('./Book/Book'));
@@ -29,80 +30,82 @@ const Layout: React.FunctionComponent<Props> = () => {
         <BrowserRouter>
           <>
             <Header />
-            <Switch>
-              <PrivateRoute path="/dashboard/" component={DashBoard} exact />
-              <PrivateRoute path="/dashboard/:slug" component={DashBoard} exact />
-              <Route exact path="/login">
-                <Suspense fallback={<Loader />}>
-                  <Login />
-                </Suspense>
-              </Route>
-              <Route exact path="/search">
-                <Suspense fallback={<Loader />}>
-                  <SearchResults />
-                </Suspense>
-              </Route>
-              <Route exact path="/book/:id">
-                <Suspense fallback={<Loader />}>
-                  <Book />
-                </Suspense>
-              </Route>
-              <Route exact path="/book-success">
-                <Suspense fallback={<Loader />}>
-                  <Success />
-                </Suspense>
-              </Route>
-              <Route path="/favorites">
-                <Suspense fallback={<Loader />}>
-                  <Favorites />
-                </Suspense>
-              </Route>
-              <Route path="/contact">
-                <Suspense fallback={<Loader />}>
-                  <Contact />
-                </Suspense>
-              </Route>
-              <Route exact path="/contact-success">
-                <Suspense fallback={<Loader />}>
-                  <Contact />
-                </Suspense>
-              </Route>
-              <Route path="/accommodation/details/:id">
-                <Suspense fallback={<Loader />}>
-                  <AccommodationDetails />
-                </Suspense>
-              </Route>
-              <Route path="/accommodations" exact>
-                <Suspense fallback={<Loader />}>
-                  <Accommodations />
-                </Suspense>
-              </Route>
-              <Route exact path="/accommodations/:number">
-                <Suspense fallback={<Loader />}>
-                  <Accommodations />
-                </Suspense>
-              </Route>
-              <Route path="/blog" exact>
-                <Suspense fallback={<Loader />}>
-                  <Blog />
-                </Suspense>
-              </Route>
-              <Route path="/blog/:id" exact>
-                <Suspense fallback={<Loader />}>
-                  <BlogDetails />
-                </Suspense>
-              </Route>
-              <Route exact path="/blog/page/:number">
-                <Suspense fallback={<Loader />}>
-                  <Blog />
-                </Suspense>
-              </Route>
-              <Route path="/">
-                <Suspense fallback={<Loader />}>
-                  <Home />
-                </Suspense>
-              </Route>
-            </Switch>
+            <MainContent>
+              <Switch>
+                <PrivateRoute path="/dashboard/" component={DashBoard} exact />
+                <PrivateRoute path="/dashboard/:slug" component={DashBoard} exact />
+                <Route exact path="/login">
+                  <Suspense fallback={<Loader />}>
+                    <Login />
+                  </Suspense>
+                </Route>
+                <Route exact path="/search">
+                  <Suspense fallback={<Loader />}>
+                    <SearchResults />
+                  </Suspense>
+                </Route>
+                <Route exact path="/book/:id">
+                  <Suspense fallback={<Loader />}>
+                    <Book />
+                  </Suspense>
+                </Route>
+                <Route exact path="/book-success">
+                  <Suspense fallback={<Loader />}>
+                    <Success />
+                  </Suspense>
+                </Route>
+                <Route path="/favorites">
+                  <Suspense fallback={<Loader />}>
+                    <Favorites />
+                  </Suspense>
+                </Route>
+                <Route path="/contact">
+                  <Suspense fallback={<Loader />}>
+                    <Contact />
+                  </Suspense>
+                </Route>
+                <Route exact path="/contact-success">
+                  <Suspense fallback={<Loader />}>
+                    <Contact />
+                  </Suspense>
+                </Route>
+                <Route path="/accommodation/details/:id">
+                  <Suspense fallback={<Loader />}>
+                    <AccommodationDetails />
+                  </Suspense>
+                </Route>
+                <Route path="/accommodations" exact>
+                  <Suspense fallback={<Loader />}>
+                    <Accommodations />
+                  </Suspense>
+                </Route>
+                <Route exact path="/accommodations/:number">
+                  <Suspense fallback={<Loader />}>
+                    <Accommodations />
+                  </Suspense>
+                </Route>
+                <Route path="/blog" exact>
+                  <Suspense fallback={<Loader />}>
+                    <Blog />
+                  </Suspense>
+                </Route>
+                <Route path="/blog/:id" exact>
+                  <Suspense fallback={<Loader />}>
+                    <BlogDetails />
+                  </Suspense>
+                </Route>
+                <Route exact path="/blog/page/:number">
+                  <Suspense fallback={<Loader />}>
+                    <Blog />
+                  </Suspense>
+                </Route>
+                <Route path="/">
+                  <Suspense fallback={<Loader />}>
+                    <Home />
+                  </Suspense>
+                </Route>
+              </Switch>
+            </MainContent>
             <Footer />
           </>
         </BrowserRouter>

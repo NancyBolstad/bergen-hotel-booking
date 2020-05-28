630c485b2ba07d376f6031d7acaeb8835c9ba600
NancyBolstad <litchacha@gmail.com>
2020-05-28 23:39:37 +0200

    Clean

--- client/src/components/Blog/styles.ts
+++ client/src/components/Blog/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import createFontStyles from '../../util/createFontStyles';
 import Typography from '../Typography';
 

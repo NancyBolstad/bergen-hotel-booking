b52890468dcf7ee64d729fc52909f35b357b9ab3
NancyBolstad <litchacha@gmail.com>
2020-05-28 13:13:59 +0200

    Added some text content

---
+++ client/src/data/content.ts
@@ -0,0 +1,48 @@
+export const PAGE_HERO_IMG = {
+  url:
+    'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589114470/holidaza/photo-1580946443359-1126222f9224_fjvl7g.jpg',
+  alt: 'Bergen scenery',
+};
+
+export const gird = {
+  url:
+    'https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1080&q=80',
+  alt: 'Foo',
+  caption:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+};
+
+export const CAMPAIGN_A = {
+  backgroundImage: {
+    url:
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589485086/holidaza/hotel/photo-1496417263034-38ec4f0b665a_psbeaj.jpg',
+    alt: 'campaign image for free breakfast',
+  },
+  text: 'Free breakfast',
+  redirectLink: `/accommodations?category=&name=&service=free%20breakfast`,
+};
+
+export const FLEX_BANNER_A = {
+  title: 'Stories from Bergen',
+  descriptions:
+    'Stay updated with the latest travel stories, tips and insights shared by our community.',
+  figure: {
+    url:
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589461864/holidaza/photo-1520769669658-f07657f5a307_kshoqw.jpg',
+    alt: 'Stories from bergen',
+  },
+  buttonText: 'Read travel blogs',
+  redirectLink: '/blog',
+};
+
+export const FLEX_BANNER_B = {
+  title: 'Why should you booking with us',
+  descriptions: 'Our holiday booking system provides the best accommodations at the best price.',
+  figure: {
+    url:
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/q_auto:good/v1590658612/holidaza/bruce-mars-ieIY61ZHhs8-unsplash_uejrjp.jpg',
+    alt: 'Services illustration',
+  },
+  buttonText: 'More about us',
+  redirectLink: '/about',
+};

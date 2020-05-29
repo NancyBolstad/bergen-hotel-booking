f1606b29b02dfda6856b5207ff397d820e5ebe9c
NancyBolstad <litchacha@gmail.com>
2020-05-29 02:03:26 +0200

    Changed icon size

--- client/src/components/IconList/styles.ts
+++ client/src/components/IconList/styles.ts
@@ -34,19 +34,17 @@ export const Item = styled(FlexKid)`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: calc(${props => props.theme.spacing.xs}rem / 2);
   text-decoration: none;
   margin: ${props => props.theme.spacing.m}rem 0;
   text-align: center;
 
   svg {
-    height: 3.625rem;
-    width: 3.937rem;
+    height: 2.625rem;
+    width: 2.937rem;
     fill: ${props => props.theme.colors.primary};
   }
 
   @media screen and (min-width: 768px) {
     width: 33.333%;
-    padding: ${props => props.theme.spacing.m}rem;
   }
 `;

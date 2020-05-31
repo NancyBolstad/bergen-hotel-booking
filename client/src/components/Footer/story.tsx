a1356777129c115459dc7886e26e6b8641b35dda
NancyBolstad <litchacha@gmail.com>
2020-05-31 16:29:38 +0200

    Rename component

--- client/src/components/Footer/story.tsx
+++ client/src/components/Footer/story.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import Footer from './';
+import Footer from './Footer';
 
 storiesOf('Component/Footer', module).add('Default', () => <Footer />);

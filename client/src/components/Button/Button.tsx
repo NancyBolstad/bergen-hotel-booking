979758e4507cde793066d22f3dc6adc9753d99df
NancyBolstad <litchacha@gmail.com>
2020-06-02 17:02:37 +0200

    Added new button variant

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -2,12 +2,17 @@ import { Link } from 'react-router-dom';
 import { Button } from './styles';
 
 export type Size = 'small' | 'medium' | 'large';
-export type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'secondaryVariant';
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'secondaryVariant'
+  | 'tertiaryVariant';
 
 export interface ButtonProps {
   size: Size;
   variant: ButtonVariant;
-  iconButton?: boolean;
+  icon?: boolean;
 }
 
 const ButtonLink = Button.withComponent('a');
--- client/src/components/Button/styles.tsx
+++ client/src/components/Button/styles.tsx
@@ -23,7 +23,13 @@ export const Button = styled.button<ButtonProps>`
   svg {
     width: 24px;
     height: 24px;
-    margin-left: ${props => (props.iconButton ? '0' : props.theme.spacing.xs)}rem;
+    margin-left: ${props => props.theme.spacing.xs}rem;
+
+    ${props =>
+      props.icon &&
+      css`
+        margin-left: 0rem;
+      `}
   }
   ${props =>
     props.size === 'small' &&
@@ -127,4 +133,23 @@ export const Button = styled.button<ButtonProps>`
         opacity: 0.8;
       }
     `};
+
+  ${props =>
+    props.variant === 'tertiaryVariant' &&
+    css`
+      background-color: transparent;
+      color: ${props => props.theme.colors.onBackground};
+      border: none;
+      &:hover,
+      &:active,
+      &:focus {
+        opacity: 0.8;
+        svg {
+          fill: ${props => props.theme.colors.primary};
+        }
+      }
+      svg {
+        fill: ${props => props.theme.colors.onBackground};
+      }
+    `}
 `;

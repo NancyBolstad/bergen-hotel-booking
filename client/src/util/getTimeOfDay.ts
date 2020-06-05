3f4767f6f0111f3a6d2461aac901199fffdba503
NancyBolstad <litchacha@gmail.com>
2020-06-05 16:24:06 +0200

    Added getTimeOfDay util

---
+++ client/src/util/getTimeOfDay.ts
@@ -0,0 +1,23 @@
+function getTimeOfDay(): string {
+  const hour = new Date().getHours();
+
+  switch (hour) {
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+      return 'Morning';
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+      return 'Afternoon';
+    default:
+      return 'Evening';
+  }
+}
+
+export default getTimeOfDay;

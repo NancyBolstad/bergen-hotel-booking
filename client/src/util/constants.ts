dc61fd146ab6cfd017df335d0d6f917655eb7130
NancyBolstad <litchacha@gmail.com>
2020-06-11 11:23:47 +0200

    Added new images

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -81,19 +81,30 @@ export const CAMPAIGN_A = {
   backgroundImage: {
     url:
       'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589485086/holidaza/hotel/photo-1496417263034-38ec4f0b665a_psbeaj.jpg',
-    alt: 'campaign image for free breakfast',
+    alt: 'campaign image for hostels',
   },
-  text: 'Free breakfast',
-  redirectLink: `/accommodations?category=&name=&service=free%20breakfast`,
+  text: 'Popular Budget Hostels',
+  redirectLink: `/accommodations?category=hostel&name=&service=`,
 };
 
 export const CAMPAIGN_B = {
   backgroundImage: {
-    url: 'http://localhost:3000/accommodations?category=hostels&name=&service=',
-    alt: 'campaign image for free breakfast',
+    url:
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/q_auto:eco/v1591865909/holidaza/photo-1517833969405-d4a24c2c8280_j3wnle.jpg',
+    alt: 'campaign image for breakfast',
   },
-  text: 'Free breakfast',
+  text: 'Best Buffet Breakfast',
   redirectLink: `/accommodations?category=&name=&service=free%20breakfast`,
+};
+
+export const CAMPAIGN_C = {
+  backgroundImage: {
+    url:
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589485087/holidaza/hotel/photo-1535209252068-f498eac4aaa3_bhdmee.jpg',
+    alt: 'campaign image for hotels',
+  },
+  text: 'Best Luxury Hotels',
+  redirectLink: `/accommodations?category=hotel&name=&service=`,
 };
 
 export const FLEX_BANNER_A = {
@@ -127,8 +138,8 @@ export const ABOUT_A = {
     'Our workers are down-to-earth, trustworthy and reliable. With us you can focus on enjoying the stay, and rest assured that your booked hotel will fullfil your expectations.',
   figure: {
     url:
-      'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589114470/holidaza/photo-1580946443359-1126222f9224_fjvl7g.jpg',
-    alt: 'Bergen scenery',
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/q_auto:eco/v1591866991/holidaza/photo-1522202176988-66273c2fd55f_1_mqvkmg.jpg',
+    alt: 'Our company',
   },
   buttonText: 'Explore our services',
   redirectLink: '/accommodations?category=&name=&service=',
@@ -140,7 +151,7 @@ export const ABOUT_B = {
     'Stay updated with the latest travel stories, tips and insights shared by our community.',
   figure: {
     url:
-      'https://res.cloudinary.com/dykdxdtuu/image/upload/v1589114470/holidaza/photo-1580946443359-1126222f9224_fjvl7g.jpg',
+      'https://res.cloudinary.com/dykdxdtuu/image/upload/q_auto:eco/v1589461866/holidaza/photo-1577453190736-43ac18f220e0_fj21l9.jpg',
     alt: 'Bergen scenery',
   },
   buttonText: 'Read our stories',
